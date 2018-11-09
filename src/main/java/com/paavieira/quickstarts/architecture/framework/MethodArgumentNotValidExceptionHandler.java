351eb5977b9e3161a9ecc43ae02636d35ed2a4b0
Paulo Andre Araujo Vieira <paulo-andre.vieira@serpro.gov.br>
2018-11-09 14:20:37 -0300

    Refactoring Exception Handler

--- src/main/java/com/paavieira/quickstarts/architecture/framework/MethodArgumentNotValidExceptionHandler.java
+++ src/main/java/com/paavieira/quickstarts/architecture/framework/MethodArgumentNotValidExceptionHandler.java
@@ -3,11 +3,13 @@ package com.paavieira.quickstarts.architecture.framework;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,24 +24,42 @@ public class MethodArgumentNotValidExceptionHandler {
     @ResponseBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Error methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
 		final BindingResult result = ex.getBindingResult();
-		final int errorCount = result.getErrorCount();
-        final List<String> globalErrors = result.getGlobalErrors().stream().map(ge -> ge.getDefaultMessage()).collect(Collectors.toList());
-        final List<String> fieldErrors = result.getFieldErrors().stream().map(fe -> fe.getDefaultMessage()).collect(Collectors.toList());
-        return new Error(BAD_REQUEST.value(), errorCount, globalErrors, fieldErrors);
+
+		final int globalErrorCount = result.getGlobalErrorCount();
+		final List<String> globalErrors = result.getGlobalErrors().stream()
+			.map(ge -> ge.getDefaultMessage()).collect(Collectors.toList());
+
+		final int fieldErrorCount = result.getFieldErrorCount();
+		final Map<String, String> fieldErrors = result.getFieldErrors().stream()
+			.collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+		return new Error(BAD_REQUEST.value(), globalErrorCount, globalErrors, fieldErrorCount, fieldErrors);
+
     }
 
     static class Error {
 
 		private final int status;
-		private final int errorCount;
-        private final List<String> globalErrors;
-        private final List<String> fieldErrors;
 
-        Error(final int status, final int errorCount, final List<String> globalErrors, final List<String> fieldErrors) {
+		private final int globalErrorCount;
+        private final List<String> globalErrors;
+
+		private final int fieldErrorCount;
+		private final Map<String, String> fieldErrors;
+
+        private Error(
+			final int status,
+			final int globalErrorCount,
+			final List<String> globalErrors,
+			final int fieldErrorCount,
+			final Map<String, String> fieldErrors
+		) {
 			this.status = status;
-			this.errorCount = errorCount;
+			this.globalErrorCount = globalErrorCount;
 			this.globalErrors = globalErrors;
+			this.fieldErrorCount = fieldErrorCount;
             this.fieldErrors = fieldErrors;
         }
 
@@ -47,15 +67,19 @@ public class MethodArgumentNotValidExceptionHandler {
             return status;
         }
 
-        public int getErrorCount() {
-            return errorCount;
+        public int getGlobalErrorCount() {
+            return globalErrorCount;
 		}
 
 		public List<String> getGlobalErrors() {
             return globalErrors;
 		}
 
-        public List<String> getFieldErrors() {
+		public int getFieldErrorCount() {
+            return fieldErrorCount;
+		}
+
+        public Map<String, String> getFieldErrors() {
             return fieldErrors;
         }
 
